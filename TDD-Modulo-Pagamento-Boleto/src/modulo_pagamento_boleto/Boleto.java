9211d7cd9283778c90ec876851e800473cb82e25
filipepiresBSI <58115159+filipepiresBSI@users.noreply.github.com>
2020-10-12 19:17:40 -0300

    Teste 3

--- TDD-Modulo-Pagamento-Boleto/src/modulo_pagamento_boleto/Boleto.java
+++ TDD-Modulo-Pagamento-Boleto/src/modulo_pagamento_boleto/Boleto.java
@@ -1,11 +1,16 @@
 package modulo_pagamento_boleto;
 
+import java.util.Random;
+
 public class Boleto {
 	
-    private String codigo = "1234567";
+	private Random rand = new Random();
+	private Integer code = rand.nextInt(10000000);
+	
+    private String codigo = code.toString();
     private String data = null;
     private double valor = 0.00;
-	
+    
     public double getValor() {
 		return valor;
 	}
